5ce4c8421ca404d0efd98f07a5f48e26a14e689d
Minh Sang <minhsangdo2308@gmail.com>
2024-10-10 22:11:45 +0700

    commit build and dist

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,4 @@
+import sys
 import speech_recognition as sr
 import os
 import requests
@@ -7,13 +8,16 @@ from wit import Wit
 
 WIT_TOKEN = 'DGH7KYZGRV5ZZMEAJQOIGJUXHYYSPKAF'
 client = Wit(WIT_TOKEN)
+username = input('Xin nhập tên của bạn : ')
 def recognize_speech():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
-        recognizer.adjust_for_ambient_noise(source, duration =1)
+        recognizer.adjust_for_ambient_noise(source)
         print('Xin mời nói: ')
-        audio = recognizer.listen(source, timeout=5, phrase_time_limit=5)
+        audio = recognizer.listen(source)
+        
     try:
+       
         text = recognizer.recognize_google(audio, language = 'vi-VN')
         print('Bạn đã nói : ' + text)
         return text
@@ -23,16 +27,17 @@ def recognize_speech():
     except sr.RequestError as e:
         print(f'Không thể hiểu yêu cầu dịch vụ google {e}')
         return None
+    
+
 
 def msg_to_wit(text):
-    url = f'https://api.wit.ai/message?v=20240304&q={text}'
+    msg_url = f'https://api.wit.ai/message?v=20240304&q={text}'
     headers = {
         'Authorization' : f'Bearer {WIT_TOKEN}'
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(msg_url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        print('Phản hồi từ WIT ', data)
         return data
     else:
         print(f'Lỗi khi gửi yêu cầu {response.status_code}')
@@ -50,20 +55,30 @@ def handle_intent(intent):
     if intent == "open_youtube":
         print('Mở youtube bằng chrome')
         os.system('start chrome https://www.youtube.com')
-    elif intent == "open chrome":
+    elif intent == "open_chrome":
         print('Mở chrome')
-        os.system("start chrome")
+        os.system("start chrome https://www.google.com")
+    elif intent == "byebye":
+        print('Hẹn gặp lại...')
+        sys.exit(0)
+    elif intent == "hello":
+        print('Chào bạn : ' + username)
     else:
         print('Intent không nhận diện được')
 
 def main():
     while True:
+        print('Đang lắng nghe : ...')
         command = recognize_speech()
+       
+        
         if command:
+            
             wit_response = msg_to_wit(command)
             intent = process_wit_response(wit_response)
             print(f'Intent nhận diện {intent}')
             handle_intent(intent)
+            
 
 if __name__ == "__main__":
     main()
